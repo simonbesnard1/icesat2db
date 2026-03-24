befeb64d03f597c5747b5b1194eb6f1d1ec81d97
felixd <felixd@gfz-potsdam.de>
2026-03-24 14:16:08 +0100

    fix: update contact information and copyright statements across multiple files

--- doc/gallery/get_variable_list.py
+++ doc/gallery/get_variable_list.py
@@ -1,3 +1,12 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Contact: felixd@gfz.de, besnard@gfz.de and urbazaev@gfz.de and amelia.holcomb@gmail.com.
+# SPDX-FileCopyrightText: 2026 Felix Dombrowski
+# SPDX-FileCopyrightText: 2026 Mikhail Urbazaev
+# SPDX-FileCopyrightText: 2026 Simon Besnard
+# SPDX-FileCopyrightText: 2026 Amelia Holcomb
+# SPDX-FileCopyrightText: 2026 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
+
+
 """
 Retrieve List of Variables in TileDB Array
 ==========================================
--- icesat2db/downloader/__init__.py
+++ icesat2db/downloader/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: EUPL-1.2
-# Contact: besnard@gfz.de, felixd@gfz.de and urbazaev@gfz.de
+# Contact: felixd@gfz.de, besnard@gfz.de and urbazaev@gfz.de and amelia.holcomb@gmail.com.
 # SPDX-FileCopyrightText: 2026 Felix Dombrowski
 # SPDX-FileCopyrightText: 2026 Mikhail Urbazaev
 # SPDX-FileCopyrightText: 2026 Simon Besnard
+# SPDX-FileCopyrightText: 2026 Amelia Holcomb
 # SPDX-FileCopyrightText: 2026 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
