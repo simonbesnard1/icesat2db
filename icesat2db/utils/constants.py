c480deec718ca6aca79bbe49690d9d3f2ef377f6
felixd <felixd@gfz-potsdam.de>
2026-03-24 14:32:24 +0100

    updated license header

--- icesat2db/utils/constants.py
+++ icesat2db/utils/constants.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: EUPL-1.2
-# Contact: felixd@gfz.de, besnard@gfz.de and urbazaev@gfz.de and amelia.holcomb@gmail.com.
+# Contact: felixd@gfz.de, besnard@gfz.de, urbazaev@gfz.de and amelia.holcomb@gmail.com.
 # SPDX-FileCopyrightText: 2026 Felix Dombrowski
 # SPDX-FileCopyrightText: 2026 Mikhail Urbazaev
 # SPDX-FileCopyrightText: 2026 Simon Besnard
